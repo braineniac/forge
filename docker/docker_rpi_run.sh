ace691076f2cad67facacee50f6d60753202bda7
Daniel Hammer <braineniac@protonmail.com>
2019-03-26 20:54:52 +0100

    Build raspi3 image

--- docker/docker_rpi_run.sh
+++ docker/docker_rpi_run.sh
@@ -2,9 +2,8 @@
 
 docker run -dit \
 				--name forge \
-				--mount type=bind,source="$(pwd)"/../../poky,target=/yocto/poky \
-				--mount type=bind,source="$(pwd)"/../../source,target=/yocto/source \
-				--mount type=bind,source="$(pwd)"/../../sstate,target=/yocto/sstate \
-				--mount type=bind,source="$(pwd)"/../../layers,target=/yocto/layers \
+				--mount type=bind,source="$(pwd)"/../source,target=/yocto/source \
+				--mount type=bind,source="$(pwd)"/../sstate,target=/yocto/sstate \
+				--mount type=bind,source="$(pwd)"/../layers,target=/yocto/layers \
 				--mount type=bind,source="$(pwd)"/../rpi,target=/yocto/rpi \
 				forge:latest
